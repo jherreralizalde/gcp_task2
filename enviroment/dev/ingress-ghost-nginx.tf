6a9c276abdec71b6e3b30294f3687a63d7dd1c5b
Jose Herrera <jose_herrera@epam.com>
2021-12-21 10:49:39 -0600

    update of files

---
+++ enviroment/dev/ingress-ghost-nginx.tf
@@ -0,0 +1,43 @@
+resource "kubernetes_ingress_v1" "example_ingress" {
+  metadata {
+    name = "ingress-nginx-ghost"
+  }
+
+  spec {
+    rule {
+      host = "ghost.test"  
+      http {
+        path {
+          backend {
+            service {
+              name = "ghost"
+              port {
+                number = 80
+              }
+            }
+          }
+
+          path = "/*"
+        }
+      }
+    }
+  rule {
+      host = "nginx.test"  
+      http {
+        path {
+          backend {
+            service {
+              name = "nginx"
+              port {
+                number = 80
+              }
+            }
+          }
+
+          path = "/*"
+        }
+      }
+    }
+  }
+}
+
